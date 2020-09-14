79555c89a79168958ab83a195d0bdeed42b77733
Warren Niu <warrenniu@gmail.com>
2020-09-14 22:00:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,12 +11,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 {:railroads => {
-  {:pieces => 4},
-  {:rent_in_dollars => {}},
-  {:names => {}}
+  :pieces => 4,
+  :rent_in_dollars => {},
+  :names => {}
+}
 }
 end
-}
 
 
 
